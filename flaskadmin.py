39afbcbefeab81d2429a73c892fa3714f17fb180
Giblegible <s3630065@student.rmit.edu.au>
2019-04-25 00:51:10 +1000

    Update bug fix

--- flaskadmin.py
+++ flaskadmin.py
@@ -1,38 +1,41 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'a33a500b00f1dec86fd7da9a48954ee9'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a33a500b00f1dec86fd7da9a48954ee'
 
 posts = [
     {
-        'author': 'Andreas Tjendra',
-        'title': 'Book 1',
-        'content': 'Study content',
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
         'author': 'Jane Doe',
-        'title': 'Harry Potter',
-        'content': 'Test content',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
         'date_posted': 'April 21, 2018'
     }
 ]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash('Account created for {form.username.data}!', 'success')
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
@@ -48,6 +51,6 @@ def login():
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run(host = '0.0.0.0', port = 80)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,22 +1,22 @@
-from flask_wtf import Form
-from wtforms import TextField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import Required, Length, Email, EqualTo
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = TextField('Username',
-                           validators=[Required(), Length(min=2, max=20)])
-    email = TextField('Email',
-                        validators=[Required(), Email()])
-    password = PasswordField('Password', validators=[Required()])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[Required(), EqualTo('password')])
+                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
-    email = TextField('Email',
-                        validators=[Required(), Email()])
-    password = PasswordField('Password', validators=[Required()])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
